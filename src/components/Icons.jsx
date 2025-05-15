7ef4a6dab074f0391e367c0f9090f6bf47e92529
Motoshi Suzuki <101000230+motoshi-suzuki-hp@users.noreply.github.com>
2025-05-15 23:29:13 +0900

    delete functions

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,15 +1,100 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { HiOutlineChat, HiOutlineHeart, HiOutlineTrash } from "react-icons/hi";
+import {
+  HiOutlineChat,
+  HiOutlineHeart,
+  HiOutlineTrash,
+  HiHeart,
+} from "react-icons/hi";
 
-export default function Icons() {
+import { useSession } from "next-auth/react";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getFirestore,
+  onSnapshot,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
+import { app } from "../firebase";
+
+export default function Icons({ id, uid }) {
+  const { data: session } = useSession();
+  const [isLiked, setIsLiked] = useState(false);
+  const [likes, setLikes] = useState([]);
+  const db = getFirestore(app);
+  const likePost = async () => {
+    if (session) {
+      if (isLiked) {
+        await deleteDoc(doc(db, "posts", id, "likes", session.user.uid));
+      } else {
+        await setDoc(doc(db, "posts", id, "likes", session.user.uid), {
+          username: session.user.username,
+          timestamp: serverTimestamp(),
+        });
+      }
+    } else {
+      signIn();
+    }
+  };
+
+  useEffect(() => {
+    onSnapshot(collection(db, "posts", id, "likes"), (snapshot) => {
+      setLikes(snapshot.docs);
+    });
+  }, [db]);
+
+  useEffect(() => {
+    setIsLiked(likes.findIndex((like) => like.id === session?.user.uid) !== -1);
+  }, [likes]);
+
+  const deletePost = async () => {
+    if (window.confirm("Are you sure you want to delete this post?")) {
+      if (session?.user.uid === uid) {
+        deleteDoc(doc(db, "posts", id))
+          .then(() => {
+            console.log("Document successfully deleted!");
+            window.location.reload();
+          })
+          .caatch((error) => {
+            console.error("Error removing document: ", error);
+          });
+      } else {
+        alert("You are not authorized to delete this post.");
+      }
+    }
+  };
+
   return (
     <div className="flex justify-start gap-5 p-2 text-gray-500">
       <HiOutlineChat className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100" />
-      <HiOutlineHeart className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100" />
-      <HiOutlineTrash className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100" />
+      <div className="flex items-center">
+        {isLiked ? (
+          <HiHeart
+            onClick={likePost}
+            className="h-8 w-8 cursor-pointer rounded-full transition text-red-600 duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
+          />
+        ) : (
+          <HiOutlineHeart
+            onClick={likePost}
+            className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
+          />
+        )}
+        {likes.length > 0 && (
+          <span className={`text-xs ${isLiked && "text-red-600"}`}>
+            {likes.length}
+          </span>
+        )}
+      </div>
 
+      {session?.user.uid === uid && (
+        <HiOutlineTrash
+          className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
+          onClick={deletePost}
+        />
+      )}
     </div>
   );
 }
